a2ec25b75ad0658025db5bde4a97b8bb9e7fc3f4
Luka Schulz <giggles14@verizon.net>
2017-12-31 13:17:49 -0500

    Added utility.js and vector.js

---
+++ js/utility.js
@@ -0,0 +1,20 @@
+const colors = {
+  WHITE: "#FAFAFA",
+  BLACK: "#121212",
+  RED: "#FF003D",
+  YELLOW: "#FFD500",
+  BLUE: "#0097FF",
+  GREEN: "#00C266",
+  ORANGE: "#FF2400",
+}
+
+let center = {
+  X: window.innerWidth/2,
+  Y: window.innerHeight/2
+}
+
+function random(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
