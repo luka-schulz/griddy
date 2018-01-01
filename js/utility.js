7d5919e822aaa63511b6319fd75aaa4a78d9caad
Luka Schulz <giggles14@verizon.net>
2017-12-31 22:41:27 -0500

    Added functions to check keystrokes

--- js/utility.js
+++ js/utility.js
@@ -23,3 +23,26 @@ function clearBackground() {
   ctx.fillStyle = colors.BLACK;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+
+function checkKeystroke(event) {
+  event = event || window.event; // IE
+  
+  if(event.keyCode === 32 || event.which === 32) {
+    toggleVisualizer();
+  }
+  else if(event.keyCode === 99 || event.which === 99) {
+    toggleControls();
+  }
+  else {
+    console.log("Press 'SPACE' to begin.")
+    console.log("Press 'c' to toggle controls.")
+  }
+};
+
+function toggleVisualizer() {
+  console.log( "Space" )
+}
+
+function toggleControls(keystroke) {
+  console.log( "c" )
+}
